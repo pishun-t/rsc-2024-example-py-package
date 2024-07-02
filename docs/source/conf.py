decd2c25b5362bb02d433d882fb95b55235b77f5
Pishun Tantivangphaisal <pishun.t@gmail.com>
2024-07-02 16:49:24 +0100

    Added sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RSC_2024_civeng'
+copyright = '2024, Pishun T'
+author = 'Pishun T'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme', # https://sphinx-rtd-theme.readthedocs.io/en/stable/installing.html
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme" # https://sphinx-rtd-theme.readthedocs.io/en/stable/installing.html
+html_static_path = ['_static']
